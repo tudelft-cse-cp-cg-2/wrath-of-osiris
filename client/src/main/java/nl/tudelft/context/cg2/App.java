6dcfb4367922a81a48db3682ac0b149702b1c6ed
mmassar <m.p.massar@student.tudelft.nl>
2020-05-01 13:27:58 +0200

    initial client boilerplate

---
+++ client/src/main/java/nl/tudelft/context/cg2/App.java
@@ -0,0 +1,35 @@
+package nl.tudelft.context.cg2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("helloworld"));
+        scene.getStylesheets().add(App.class.getResource("styles.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ client/src/main/java/nl/tudelft/context/cg2/HelloWorldController.java
@@ -0,0 +1,7 @@
+package nl.tudelft.context.cg2;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class HelloWorldController {
+}
