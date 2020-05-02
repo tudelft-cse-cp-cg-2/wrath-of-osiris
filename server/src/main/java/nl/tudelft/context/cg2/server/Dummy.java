0afe446afff697cfa80ab3ff335c1eba8b9040d0
mmassar <m.p.massar@student.tudelft.nl>
2020-05-02 13:16:01 +0200

    add scaffolding and junit for server

---
+++ server/src/main/java/nl/tudelft/context/cg2/server/Dummy.java
@@ -0,0 +1,8 @@
+package nl.tudelft.context.cg2.server;
+
+public class Dummy {
+    // dummy code
+    public static int inc(int n) {
+        return n + 1;
+    }
+}
---
+++ server/src/test/java/nl/tudelft/context/cg2/server/DummyTest.java
@@ -0,0 +1,12 @@
+package nl.tudelft.context.cg2.server;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DummyTest {
+    @Test
+    public void testInc() {
+        assertEquals(2, Dummy.inc(1));
+    }
+}
