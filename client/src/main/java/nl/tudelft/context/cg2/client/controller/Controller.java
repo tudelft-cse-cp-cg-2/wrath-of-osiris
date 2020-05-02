936e83368664dc9f2365bdcd278912b6d8dd222c
Niels Mook <n.l.c.mook@student.tudelft.nl>
2020-05-02 22:13:22 +0200

    Fix Controller class checkstyle errors

--- client/src/main/java/nl/tudelft/context/cg2/client/controller/Controller.java
+++ client/src/main/java/nl/tudelft/context/cg2/client/controller/Controller.java
@@ -3,12 +3,21 @@ package nl.tudelft.context.cg2.client.controller;
 import nl.tudelft.context.cg2.client.model.Model;
 import nl.tudelft.context.cg2.client.view.View;
 
+/**
+ * Controller class representing the Controller in the View-Controller-Model design pattern.
+ * It handles user input and sends it to the model.
+ */
 public class Controller {
 
     private final Model model;
     private final View view;
 
-    public Controller(Model model, View view) {
+    /**
+     * Constructor for the Controller object.
+     * @param model The Model object to send interaction data towards.
+     * @param view The View object, containing the UI controls calling back to this controller.
+     */
+    public Controller(final Model model, final View view) {
         this.view = view;
         this.model = model;
     }
