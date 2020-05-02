2571d20c7632531e267bdacf39bc52cfebe66695
mmassar <m.p.massar@student.tudelft.nl>
2020-05-02 12:32:14 +0200

    add junit5 testing

---
+++ client/src/test/java/nl/tudelft/context/cg2/ExampleTest.java
@@ -0,0 +1,12 @@
+package nl.tudelft.context.cg2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExampleTest {
+    @Test
+    public void testRunning() {
+        assertTrue(!false);
+    }
+}
