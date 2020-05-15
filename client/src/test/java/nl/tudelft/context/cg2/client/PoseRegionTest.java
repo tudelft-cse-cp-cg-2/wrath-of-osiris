a35c58cb78203e830f8f5535d1c12b0009d1a19d
mmassar <m.p.massar@student.tudelft.nl>
2020-05-15 17:07:57 +0200

    unbreak tests

--- client/src/test/java/nl/tudelft/context/cg2/client/PoseRegionTest.java
+++ client/src/test/java/nl/tudelft/context/cg2/client/PoseRegionTest.java
@@ -1,6 +1,8 @@
 package nl.tudelft.context.cg2.client;
 
+import nl.tudelft.context.cg2.client.posedetection.Limb;
 import nl.tudelft.context.cg2.client.posedetection.PoseRegion;
+import nl.tudelft.context.cg2.client.posedetection.Position;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,9 +14,9 @@ public class PoseRegionTest {
         int ty = 0;
         int bx = 250;
         int by = 250;
-        int limb = 1;
-        int option = 0;
-        PoseRegion poseRegion = new PoseRegion(tx, ty, bx, by, limb, option);
+        Limb limb = Limb.left_arm;
+        Position position = Position.middle;
+        PoseRegion poseRegion = new PoseRegion(tx, ty, bx, by, limb, position);
 
         // Test getters
         assertTrue(poseRegion.getLeftX() == tx);
@@ -22,7 +24,7 @@ public class PoseRegionTest {
         assertTrue(poseRegion.getRightX() == bx);
         assertTrue(poseRegion.getBottomY() == by);
         assertTrue(poseRegion.getLimb() == limb);
-        assertTrue(poseRegion.getOption() == option);
+        assertTrue(poseRegion.getPosition() == position);
 
         // Test Point getters
         assertTrue(poseRegion.getTopLeft() != null);
@@ -32,19 +34,19 @@ public class PoseRegionTest {
 
     @Test
     public void pointInRangeTrue() {
-        PoseRegion poseRegion = new PoseRegion(0, 0, 250, 250, 0, 0);
+        PoseRegion poseRegion = new PoseRegion(0, 0, 250, 250, Limb.left_leg, Position.neutral);
         assertTrue(poseRegion.inRange(50, 50));
     }
 
     @Test
     public void pointInRangeFalse() {
-        PoseRegion poseRegion = new PoseRegion(0, 0, 250, 250, 0, 0);
+        PoseRegion poseRegion = new PoseRegion(0, 0, 250, 250, Limb.left_leg, Position.neutral);
         assertTrue(!poseRegion.inRange(350, 350));
     }
 
     @Test
     public void pointInRangeBoundary() {
-        PoseRegion poseRegion = new PoseRegion(0, 0, 250, 250, 0, 0);
+        PoseRegion poseRegion = new PoseRegion(0, 0, 250, 250, Limb.left_leg, Position.neutral);
         assertTrue(poseRegion.inRange(50, 0));
     }
 }
--- client/src/test/java/nl/tudelft/context/cg2/client/PoseTest.java
+++ client/src/test/java/nl/tudelft/context/cg2/client/PoseTest.java
@@ -1,6 +1,8 @@
 package nl.tudelft.context.cg2.client;
 
+import nl.tudelft.context.cg2.client.posedetection.Limb;
 import nl.tudelft.context.cg2.client.posedetection.Pose;
+import nl.tudelft.context.cg2.client.posedetection.Position;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -9,72 +11,75 @@ public class PoseTest {
     @Test
     public void poseInit() {
         // options for arms
-        int leftArm = 0;
-        int rightArm = 2;
+        Position leftArm = Position.top;
+        Position rightArm = Position.bottom;
         // options for legs
-        int leftLeg = 1;
-        int rightLeg = 0;
+        Position leftLeg = Position.raised;
+        Position rightLeg = Position.neutral;
         Pose pose = new Pose(leftArm, rightArm, leftLeg, rightLeg);
         // Test pose
         assertTrue(pose.toString()
-                .equals("Pose: la: " + leftArm + "| ra: " + rightArm + "| ll: " + leftLeg + "| rl: " + rightLeg));
+                .equals("Pose: la: " + leftArm.name() + "| ra: " + rightArm.name() + "| ll: " + leftLeg.name() + "| rl: " + rightLeg.name()));
     }
 
     @Test
     public void updatePoseOnce() {
-        Pose pose = new Pose(0, 0, 0, 0);
+        Pose pose = new Pose(Position.top, Position.top, Position.neutral, Position.neutral);
         pose.resetCounters();
-        pose.incrementCounter(0, 0);
-        pose.incrementCounter(0, 1);
-        pose.incrementCounter(0, 1);
+        pose.incrementCounter(Limb.right_arm, Position.top);
+        pose.incrementCounter(Limb.right_arm, Position.middle);
+        pose.incrementCounter(Limb.right_arm, Position.middle);
         // limb 0 option 1 has most votes
-        pose.incrementCounter(1, 0);
-        pose.incrementCounter(1, 1);
-        pose.incrementCounter(1, 1);
-        pose.incrementCounter(1, 2);
-        pose.incrementCounter(1, 2);
-        pose.incrementCounter(1, 2);
+        pose.incrementCounter(Limb.left_arm, Position.top);
+        pose.incrementCounter(Limb.left_arm, Position.middle);
+        pose.incrementCounter(Limb.left_arm, Position.middle);
+        pose.incrementCounter(Limb.left_arm, Position.bottom);
+        pose.incrementCounter(Limb.left_arm, Position.bottom);
+        pose.incrementCounter(Limb.left_arm, Position.bottom);
         // limb 1 option 2 has most votes
-        pose.incrementCounter(2, 0);
-        pose.incrementCounter(2, 1);
+        pose.incrementCounter(Limb.left_leg, Position.top);
+        pose.incrementCounter(Limb.left_leg, Position.middle);
         // limb 2 option 0 and 1 have most votes
-        pose.incrementCounter(3, 0);
-        pose.incrementCounter(3, 0);
+        pose.incrementCounter(Limb.right_leg, Position.top);
+        pose.incrementCounter(Limb.right_leg, Position.top);
         // limb 3 option 0 has most votes
         pose.updatePose();
         assertTrue(pose.toString()
-                .equals("Pose: la: " + 1 + "| ra: " + 2 + "| ll: " + 1 + "| rl: " + 0));
+                .equals("Pose: la: " + Position.middle.name() + "| ra: " + Position.bottom.name()
+                        + "| ll: " + Position.middle.name() + "| rl: " + Position.top.name()));
     }
 
     @Test
     public void updatePoseTwice() {
-        Pose pose = new Pose(0, 0, 0, 0);
+        Pose pose = new Pose(Position.top, Position.top, Position.neutral, Position.neutral);
         pose.resetCounters();
-        pose.incrementCounter(0, 0);
-        pose.incrementCounter(0, 1);
-        pose.incrementCounter(0, 1);
+        pose.incrementCounter(Limb.right_arm, Position.top);
+        pose.incrementCounter(Limb.right_arm, Position.middle);
+        pose.incrementCounter(Limb.right_arm, Position.middle);
         // limb 0 option 1 has most votes
-        pose.incrementCounter(1, 0);
-        pose.incrementCounter(1, 1);
-        pose.incrementCounter(1, 1);
-        pose.incrementCounter(1, 2);
-        pose.incrementCounter(1, 2);
-        pose.incrementCounter(1, 2);
+        pose.incrementCounter(Limb.left_arm, Position.top);
+        pose.incrementCounter(Limb.left_arm, Position.middle);
+        pose.incrementCounter(Limb.left_arm, Position.middle);
+        pose.incrementCounter(Limb.left_arm, Position.bottom);
+        pose.incrementCounter(Limb.left_arm, Position.bottom);
+        pose.incrementCounter(Limb.left_arm, Position.bottom);
         // limb 1 option 2 has most votes
-        pose.incrementCounter(2, 0);
-        pose.incrementCounter(2, 1);
+        pose.incrementCounter(Limb.left_leg, Position.top);
+        pose.incrementCounter(Limb.left_leg, Position.middle);
         // limb 2 option 0 and 1 have most votes
-        pose.incrementCounter(3, 0);
-        pose.incrementCounter(3, 0);
+        pose.incrementCounter(Limb.right_leg, Position.top);
+        pose.incrementCounter(Limb.right_leg, Position.top);
         // limb 3 option 0 has most votes
         pose.updatePose();
         assertTrue(pose.toString()
-                .equals("Pose: la: " + 1 + "| ra: " + 2 + "| ll: " + 1 + "| rl: " + 0));
+                .equals("Pose: la: " + Position.middle.name() + "| ra: " + Position.bottom.name()
+                        + "| ll: " + Position.middle.name() + "| rl: " + Position.top.name()));
 
-        pose.incrementCounter(0, 0);
-        pose.incrementCounter(0, 0);
+        pose.incrementCounter(Limb.right_arm, Position.top);
+        pose.incrementCounter(Limb.right_arm, Position.top);
         pose.updatePose();
         assertTrue(pose.toString()
-                .equals("Pose: la: " + 0 + "| ra: " + 2 + "| ll: " + 1 + "| rl: " + 0));
+                .equals("Pose: la: " + Position.top.name() + "| ra: " + Position.bottom.name()
+                        + "| ll: " + Position.middle.name() + "| rl: " + Position.top.name()));
     }
 }
