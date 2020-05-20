f4e9020d2850f6a38980570e0528360d559c0c4c
Jurrien Theunisz <J.theunisz@student.tudelft.nl>
2020-05-20 17:04:46 +0200

    Adds randomizer to wall velocity.

--- client/src/main/java/nl/tudelft/context/cg2/client/model/world/factories/WallFactory.java
+++ client/src/main/java/nl/tudelft/context/cg2/client/model/world/factories/WallFactory.java
@@ -20,7 +20,7 @@ public class WallFactory {
     public static Wall generateWall() {
         Image image = ImageCache.IMAGES[0];
         Vector3D position = new Vector3D(0D, 0, World.DEPTH);
-        Vector3D velocity = new Vector3D(0D, 0D, -60D);
+        Vector3D velocity = new Vector3D(0D, 0D, Math.random() * -150D - 20D);
         Vector3D size = new Vector3D(World.WIDTH, World.HEIGHT, 0);
         return new Wall(image, position, velocity, size);
     }
