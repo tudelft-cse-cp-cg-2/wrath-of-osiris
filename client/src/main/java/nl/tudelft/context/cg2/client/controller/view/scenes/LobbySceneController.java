57f3a28abd32f26f2cfa6a142e638de0070b9a0d
Niels Mook <n.l.c.mook@student.tudelft.nl>
2020-06-22 16:15:51 +0200

    Update text

--- client/src/main/java/nl/tudelft/context/cg2/client/controller/view/scenes/LobbySceneController.java
+++ client/src/main/java/nl/tudelft/context/cg2/client/controller/view/scenes/LobbySceneController.java
@@ -58,7 +58,7 @@ public class LobbySceneController extends SceneController {
         scene.showPopup(
                 "Tips:"
                         + "\n* Make sure your entire posture is in the webcam preview."
-                        + "\n* Use ESCAPE to leave a running game."
+                        + "\n* Press ESCAPE to leave a running game."
                         + "\n* Posture is better recognized with contrasting colors compared to " +
                         "the background color(s)."
 
@@ -68,7 +68,9 @@ public class LobbySceneController extends SceneController {
 
                 + "\n\nRun for your life and reach the highest level by taking the pose "
                 + "displayed by the holes in the approaching walls, or else the pyramid "
-                + "will crumble even faster!"
+                + "will crumble even faster! "
+                + "The holes can have three positions for each arm, "
+                + "and two positions for each leg. "
 
                 + "\n\nEach level consists of several walls. "
                 + "When one of your team members fails fitting through one of the holes,"
