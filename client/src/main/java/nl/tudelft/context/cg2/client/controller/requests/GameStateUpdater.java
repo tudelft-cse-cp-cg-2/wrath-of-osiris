d846f2956c386f336f6a198bef7e7bebb47f8048
Jurrien Theunisz <J.theunisz@student.tudelft.nl>
2020-06-26 20:05:45 +0200

    Removes unused methods.

--- client/src/main/java/nl/tudelft/context/cg2/client/controller/requests/GameStateUpdater.java
+++ client/src/main/java/nl/tudelft/context/cg2/client/controller/requests/GameStateUpdater.java
@@ -31,7 +31,6 @@ public class GameStateUpdater extends Thread {
 
     /**
      * Constructor for GameStateUpdater.
-     *
      * @param in         server input
      * @param out        server output
      * @param controller app controller
@@ -40,13 +39,6 @@ public class GameStateUpdater extends Thread {
         this.in = in;
         this.out = out;
         this.controller = controller;
-    }
-
-    /**
-     * Changes this.terminate to true, effectively telling the run function to terminate.
-     */
-    public void terminate() {
-        this.terminate = true;
     }
 
     /**
@@ -265,14 +257,6 @@ public class GameStateUpdater extends Thread {
      */
     public void sendFinalPose(Pose pose) {
         out.println("finalpose " + pose.pack());
-    }
-
-    /**
-     * Getter for the started boolean.
-     * @return whether the game is started
-     */
-    public boolean isStarted() {
-        return started;
     }
 
     /**
